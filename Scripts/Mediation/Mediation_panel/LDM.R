e55208d18b0013b23f20d13e65065d51ea209d3a
Lin-LinXu <127443645+Lin-LinXu@users.noreply.github.com>
2024-11-24 17:22:05 +0100

    Update LDM.R

--- Scripts/Mediation/Mediation_panel/LDM.R
+++ Scripts/Mediation/Mediation_panel/LDM.R
@@ -1,3 +1,9 @@
+# Title: Mediation LDM
+# Project: Chemical Pollution Drives Taxonomic and Functional Shifts in Marine Sediment Microbiome, Influencing Benthic Metazoans
+# Maintainer: Lin-Lin Xu
+# Last update: Nov 24, 2024
+# License: MI
+
 ## Load library ----------------------------------------------------------------
 library(LDM)
 library(tidyverse)
